a874ad604e5a37f333fac84469b9195b0f420bbd
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-11 15:41:29 +0530

    Image Added

---
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './ImageGenerator.css'
+import default_Image from '../Assets/default_image.svg'
+
+const ImageGenerator = () => {
+    return (
+        <div className='ai-image-generator'>
+            <div className="header">Ai image <span>generator</span></div>
+            <div className="img-loading">
+                <div className="image"><img src={default_Image} alt=""/> </div>
+            </div>
+        </div>
+    )
+}
+export default ImageGenerator
