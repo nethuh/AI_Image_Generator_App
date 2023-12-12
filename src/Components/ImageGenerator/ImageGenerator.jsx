65784735b9e02de7b6553e09c814c04a6a59ab92
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-12 16:45:49 +0530

    updated

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import './ImageGenerator.css'
 import default_Image from '../Assets/default_image.svg'
 
 const ImageGenerator = () => {
+
+    const [image_url,setImage_url] = useState("/");
+    let inputRef = useRef(null);
+
+    const imageGenerator = async () =>{
+        if (inputRef.current.value===""){
+            return 0;
+        }
+        const response  = await fetch()
+    }
+
     return (
         <div className='ai-image-generator'>
             <div className="header">AI Image <span>Generator</span></div>
             <div className="img-loading">
-                <div className="image"><img src={default_Image} alt=""/> </div>
+                <div className="image"><img src={image_url==="/"?default_Image:image_url} alt=""/> </div>
 
             </div>
             <div className="search-box">
-                <input type="text" className='search-input' placeholder='Describe What You Want To See'/>
+                <input type="text" ref={inputRef} className='search-input' placeholder='Describe What You Want To See'/>
                 <div className="generate-btn">Generator</div>
 
             </div>
