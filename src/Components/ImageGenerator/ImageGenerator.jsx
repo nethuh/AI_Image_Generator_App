7fb14699fe9eea9a3f9bb9ed4351708e6da9b524
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-12 21:25:40 +0530

    updated

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -6,11 +6,13 @@ const ImageGenerator = () => {
 
     const [image_url, setImage_url] = useState("/");
     let inputRef = useRef(null);
+    const [loading,setLoading] = useState(false);
 
     const imageGenerator = async () => {
-        if (inputRef.current.value === "") {
+        if (inputRef.current.value==="") {
             return 0;
         }
+        setLoading(true);
         const response = await fetch(
             "https://api.openai.com/v1/images/generations",
             {
@@ -19,12 +21,12 @@ const ImageGenerator = () => {
                     "Content-Type": "application/json",
                     Authorization:
                         "Bearer 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",
-                    "User-Agent": "Chrome",
+                    "User-Agent": "Firefox",
 
                 },
                 body: JSON.stringify({
-                    prompt: `${inputRef.current.value}`,
-                    n: 1,
+                    prompt:`${inputRef.current.value}`,
+                    n:1,
                     size: "512x512",
                 }),
             }
@@ -32,6 +34,7 @@ const ImageGenerator = () => {
         let data = await response.json();
         let data_array = data.data;
         setImage_url(data_array[0].url);
+        setLoading(false);
     }
 
     return (
@@ -40,8 +43,8 @@ const ImageGenerator = () => {
             <div className="img-loading">
                 <div className="image"><img src={image_url === "/" ? default_Image : image_url} alt=""/></div>
                 <div className="loading">
-                    <div className="loading-bar"></div>
-                    <div className="loading-text">Loading...</div>
+                    <div className={loading?"loading-bar-full":"loading-bar"}></div>
+                    <div className={loading?"loading-text":"display-none"}>Loading...</div>
                 </div>
             </div>
             <div className="search-box">
